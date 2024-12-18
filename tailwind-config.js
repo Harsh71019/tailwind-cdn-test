6416dc07b1e2de6f8fe5ffadf923bff0ce693847
harsh71019 <harsh710patel@gmail.com>
2024-12-18 11:49:42 +0530

    feat: initial config

---
+++ tailwind-config.js
@@ -0,0 +1,56 @@
+window.SHARED_TAILWIND_CONFIG = {
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          50: '#E0E9F2',
+          100: '#6C8CB3',
+          200: '#295DA7',
+          300: '#0033FF',
+          400: '#6C80A3',
+          500: '#305C83',
+          600: '#2B477D',
+          700: '#006CFF',
+        },
+        yellow: {
+          50: '#FFFACD',
+        },
+        border: {
+          50: '#c5c5c5',
+          100: '#000',
+          200: '#999',
+        },
+        button: {
+          primary: {
+            bg: '#d4d4d4',
+            text: '#333333',
+            hoverBg: '#b9ced9',
+            border: '#C5C5C5',
+            hoverBorder: '#627782',
+          },
+          secondary: {
+            bg: '#9e9e9e',
+            text: '#333333',
+            hoverBg: '#8e8e8e',
+            border: '#c5c5c5',
+            hoverBorder: '#627782',
+          },
+        },
+      },
+      spacing: {
+        18: '18px',
+        70: '70px',
+      },
+      textShadow: {
+        default: '1px 1px #fff',
+      },
+      fontSize: {
+        btn: '12px',
+      },
+      fontFamily: {
+        arial: ['Arial'],
+      },
+    },
+  },
+  plugins: [require('tailwindcss-textshadow')],
+};
